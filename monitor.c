e09817b9cbbc4441c90c0622b34cba66262055da
Gustavo Reis <reisgustavoh@gmail.com>
2013-05-21 22:13:07 -0300

    Update monitor.c

--- monitor.c
+++ monitor.c
@@ -14,7 +14,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <time.h>
 #include "monitor.h"
 
 /* VARIAVEIS COMPARTILHADAS */
@@ -23,14 +22,22 @@ int espera[] = { 0, 0, 0, 0, 0, 0 };
 int threads_ativas = 0;
 
 /* MUTEXES E VARIAVEIS CONDICIONAIS */
-pthread_cond_t casal[3];
+pthread_cond_t casal[6];
 pthread_mutex_t monitor = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutexContador = PTHREAD_MUTEX_INITIALIZER;
 
 /* FUNCOES AUXILIARES */
+
+/*
+ * Identifica o nome do personagem a partir do identificador da Thread.
+ * Parametros:
+ *  * id - identificador da thread
+ * Retorno:
+ *  * Nome do personagem.
+ */
 char* nome(int id) {
 
-  char* nome;
+	char* nome;
 
 	switch (id) {
 	case 0:
@@ -58,12 +65,27 @@ char* nome(int id) {
 	return nome;
 }
 
+/*
+ * Verifica se existe pelo menos uma pessoa do casal aguardando para usar o forno.
+ * Parametros:
+ *  * casal - indice do casal a ser verificado.
+ * Retorno:
+ *  * 1 se tem pelo menos uma pessoa do casal em espera.
+ *  * 0 em caso contrário.
+ */
 int esta_esperando(int casal) {
 
 	return (espera[casal] != 0 || espera[casal + 3] != 0);
 
 }
 
+/*
+ * Determina o indice do casal de maior precedência, baseado no identificador da thread/personagem.
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ * Retorno:
+ *  * Inteiro com o indice do casal de maior precedência (0, 1 ou 2).
+ */
 int maior_precedencia(int id) {
 
 	int indice;
@@ -78,23 +100,61 @@ int maior_precedencia(int id) {
 
 }
 
+/*
+ * Determina o indice do casal de menor precedência, baseado no identificador da thread/personagem.
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ * Retorno:
+ *  * Inteiro com o indice do casal de menor precedência (0, 1 ou 2).
+ */
 int menor_precedencia(int id) {
 	return (id + 1) % 3;
 }
 
+/*
+ * Determina o indice do casal a qual pertence a thread/personagem.
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ * Retorno:
+ *  * Inteiro com o indice do casal. (0, 1 ou 2).
+ */
 int mesma_precedencia(int id) {
 	return (id % 3);
 }
 
 /* FUNCOES DO MONITOR */
+
+/*
+ * Verifica se o forno está disponível ou não para uso. Caso o forno esteja disponível esta função marca o mesmo para uso pela thread/personagem
+ * identificada pelo parâmetro #id. Caso o forno esteja em uso por outra thread, a thread atual registra a sua espera em uma variável de condição
+ * do casal. Para garantir que a ordem de acesso seja a ordem de chegada entre pessoas de um mesmo casal, foi utilizada uma segunda variável de condição (por casal).
+ * Quando um personagem deseja usar o forno e seu namorado(a) já está esperando ela deve aguardar na segunda variável de condição.
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ * Retorno:
+ *  * Inteiro com o indice do casal de maior precedência (0, 1 ou 2).
+ */
 void esperar(int id) {
 	pthread_mutex_lock(&monitor);
 
 	printf("%s quer usar o forno.\n", nome(id));
 
 	if (forno != 0) {
-		espera[id]++;
-		pthread_cond_wait(&(casal[id % 3]), &monitor);
+		if (!esta_esperando(id % 3)) {
+			espera[id]++;
+
+			// Aguarda na variável de condição primária do casal
+			pthread_cond_wait(&(casal[id % 3]), &monitor);
+
+		} else {
+
+			espera[id]++;
+			// Aguarda na variável de condição secundária do casal
+			pthread_cond_wait(&(casal[(id % 3) + 3]), &monitor);
+
+			// Aguarda na variável de condição primária do casal
+			pthread_cond_wait(&(casal[id % 3]), &monitor);
+		}
 	}
 
 	// Marca o forno para uso
@@ -109,6 +169,14 @@ void esperar(int id) {
 
 }
 
+/*
+ * Libera o forno e define a próxima thread a executar de acordo com a política de prioridades circular.
+ * Para evitar inanição é sempre respeitada a seguinte ordem: thread de maior prioridade > thread de menor prioridade > thread de igual prioridade.
+ * Em cada um dos casos é liberado a variável de condição primária, permitindo que a thread do casal que chegou 1º execute. Também é liberado a
+ * variável de condição secundária, permitindo que o membro do casal que chegou em 2º (caso exista), passe a ocupar a variável condicional primária.
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ */
 void liberar(int id) {
 
 	pthread_mutex_lock(&monitor);
@@ -121,18 +189,32 @@ void liberar(int id) {
 	if (!(esta_esperando(0) && esta_esperando(1) && esta_esperando(2))) {
 		if (esta_esperando(maior_precedencia(id))) {
 			pthread_cond_signal(&(casal[maior_precedencia(id)]));
+			pthread_cond_signal(&(casal[maior_precedencia(id) + 3]));
 		} else if (esta_esperando(menor_precedencia(id))) {
 			pthread_cond_signal(&(casal[menor_precedencia(id)]));
+			pthread_cond_signal(&(casal[menor_precedencia(id) + 3]));
 		} else {
 			pthread_cond_signal(&(casal[mesma_precedencia(id)]));
-
+			pthread_cond_signal(&(casal[mesma_precedencia(id) + 3]));
 		}
+	} else {
+		// Caso ocorra uma deadlock não libera ninguém, mas atualiza a posição de espera do(a) namorado(a),
+		// liberando a variável condicional secundária do casal
+		pthread_cond_signal(&(casal[mesma_precedencia(id) + 3]));
 	}
 
 	pthread_mutex_unlock(&monitor);
 
 }
 
+/*
+ * Verifica se ocorreu um deadlock e em caso positivo escolhe aleatoriamente um casal para ser liberado.
+ * Sempre é liberado o membro do casal que chegou primeiro (esperando na variável de condição primária do casal).
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ * Retorno:
+ *  * Inteiro com o indice do casal de maior precedência (0, 1 ou 2).
+ */
 void verifica() {
 
 	if (forno == 0 && esta_esperando(0) && esta_esperando(1) && esta_esperando(2)) {
@@ -141,22 +223,31 @@ void verifica() {
 		int i = rand();
 
 		pthread_cond_signal(&(casal[i % 3]));
+		pthread_cond_signal(&(casal[(i % 3) + 3]));
 
-		printf("Raj detectou um deadlock, liberando um membro do casal %d (%s e %s).\n", i % 3, nome(i % 3),
-				nome((i % 3) + 3));
+		printf("Raj detectou um deadlock, liberando um membro do casal [%s e %s].\n", nome(i % 3), nome((i % 3) + 3));
 
 	}
 
 }
 
 /* FUNCOES ESPECIFICAS DO PROBLEMA */
+
+/*
+ * Utilização do forno pelo personagem. Duração de 1 segundo, conforme especificação.
+ * Parametros:
+ *  * id - identificador da thread/personagem.
+ */
 void esquentar_algo(int id) {
 
 	printf("%s começa a esquentar algo.\n", nome(id));
 	sleep(1);
 }
 
-void comer(int tid) {
+/*
+ * Espera um tempo aleatório entre 3 e 6 segundos.
+*/
+void comer() {
 
 	srand(time(NULL ));
 	int i = rand();
@@ -166,17 +257,30 @@ void comer(int tid) {
 }
 
 /* FUNCOES RELACIONADAS AS THREADAS */
+
+/*
+ * Função para inicialização da thread do personagem Raj. Essa thread controla a ocorrência de deadlocks, chamando a função verificar a cada 5 segundos.
+ * Parametros:
+ *  * arg - argumentos para função de inicialização das threads.
+*/
 void *inicia_raj(void *arg) {
 
 	while (threads_ativas > 0) {
 		verifica();
-		sleep(3);
+		sleep(5);
 	}
 
 	pthread_exit(NULL );
 
 }
 
+
+/*
+ * Função para inicialização da threads de cada personagem (exceto Raj). Executa a chamada as funções do monitor, usando o forno de uma forma
+ * consistente.
+ * Parametros:
+ *  * arg - argumentos para função de inicialização das threads. Usado para recuperar o identificador de cada thread e o número de iterações.
+*/
 void *inicia_casais(void *arg) {
 
 	pthread_mutex_lock(&mutexContador);
@@ -190,7 +294,7 @@ void *inicia_casais(void *arg) {
 		esperar((*t).id);
 		esquentar_algo((*t).id);
 		liberar((*t).id);
-		comer((*t).id);
+		comer();
 
 		(*t).num_iteracoes--;
 	}
